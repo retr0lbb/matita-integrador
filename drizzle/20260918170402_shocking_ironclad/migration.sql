8a514135ab8753d6e90c3d91e62eb3aac381704c
retr0lbb <retr0lbb@gmail.com>
2026-09-18 14:04:50 -0300

    minor change

---
+++ drizzle/20260918170402_shocking_ironclad/migration.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "classrooms" DROP CONSTRAINT "classrooms_google_classroom_id_key";--> statement-breakpoint
+ALTER TABLE "classrooms" DROP CONSTRAINT "classrooms_title_key";
--- src/database/schemas/classRoomSchema.ts
+++ src/database/schemas/classRoomSchema.ts
@@ -11,11 +11,11 @@ export const classRoomStatus = pgEnum("classroom_status", [
 
 export const classRoomTable = pgTable("classrooms", {
     id: uuid().defaultRandom().primaryKey(),
-    googleClassroomId: varchar("google_classroom_id").unique(),
+    googleClassroomId: varchar("google_classroom_id"),
     externalId: varchar("external_id"),
     ownerId: uuid("owner_id").notNull().references(() => accountTable.id),
     unitId: uuid("unit_id").notNull().references(() => unitTable.id),
-    title: varchar().notNull().unique(),
+    title: varchar().notNull(),
     status: classRoomStatus().default("PENDING")
 }, (table) => [
     unique("classroom_google_id_external_id").on(table.googleClassroomId, table.externalId)
